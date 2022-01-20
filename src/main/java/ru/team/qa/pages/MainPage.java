35881d4aa32acc51e6f30e0c9bb00e1b605ce5ef
Ксения Тапок <soulssfg@gmail.com>
2022-01-20 16:33:41 +0300

    add steps

--- src/main/java/ru/team/qa/pages/MainPage.java
+++ src/main/java/ru/team/qa/pages/MainPage.java
@@ -5,10 +5,22 @@ import cucumber.api.java.ru.*;
 
 public class MainPage {
 
-    @Когда("^нажать на кнопку (.+)")
-    @When("^push button (.+)")
+    @Когда("^открыть главную страницу$")
+    @When("^open main page$")
+    public static void openMain() {
+
+    }
+
+    @Когда("^нажать на кнопку (.+)$")
+    @When("^push button (.+)$")
     public static void clickButton(String button) {
         // тут селениумом надо сделать клик по кнопке
         // в идеале бы сделать это через super-методы и @Override, можно попробовать разобраться
+    }
+
+    @Когда("^заполнить поле (.+) текстом (.+)$")
+    @When("^fill in the field (.+) with text (.+)$")
+    public static void putText() {
+
     }
 }
